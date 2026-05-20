9d49c6ffc508a9305136da99ef6e02027983d11a
Abhay Bhabhu <ne22606@bristol.ac.uk>
2026-05-20 14:44:49 +0100

    writing to micro sd test

--- firmware/xiao_microsd_write_test/xiao_microsd_write_test.ino
+++ firmware/xiao_microsd_write_test/xiao_microsd_write_test.ino
@@ -1,56 +1,33 @@
-/*
-  TriSense Test 003 - microSD Write Test
-
-  Purpose:
-  Check whether the XIAO nRF52840 Sense can write a basic text file
-  to the microSD card over SPI.
-
-  Wiring:
-  microSD GND  -> XIAO GND
-  microSD VCC  -> XIAO 3V3
-  microSD MISO -> XIAO D9
-  microSD MOSI -> XIAO D10
-  microSD SCK  -> XIAO D8
-  microSD CS   -> XIAO D7
-*/
-
 #include <SPI.h>
 #include <SD.h>
 
-const int chipSelect = D7;
+const int chipSelect = 5;
 
 void setup() {
   Serial.begin(115200);
+  while (!Serial) delay(10);
 
-  while (!Serial) {
-    delay(10);
-  }
+  Serial.println("TriSense SD basic init test");
 
-  Serial.println("TriSense microSD write test starting...");
+  pinMode(chipSelect, OUTPUT);
+  digitalWrite(chipSelect, HIGH);
 
   if (!SD.begin(chipSelect)) {
-    Serial.println("SD card initialisation failed.");
-    Serial.println("Check wiring, card format, and CS pin.");
-    while (1) {
-      delay(1000);
-    }
+    Serial.println("SD.begin failed");
+    return;
   }
 
-  Serial.println("SD card initialised successfully.");
+  Serial.println("SD.begin passed");
 
   File testFile = SD.open("test.txt", FILE_WRITE);
 
   if (testFile) {
-    testFile.println("TriSense Prototype 0.1 microSD write test");
-    testFile.println("If you can read this, the XIAO wrote to the SD card successfully.");
+    testFile.println("TriSense SD write test passed.");
     testFile.close();
-
     Serial.println("test.txt written successfully.");
   } else {
-    Serial.println("Failed to open test.txt for writing.");
+    Serial.println("Could not open test.txt");
   }
 }
 
-void loop() {
-  // Nothing needed for this test.
-}
+void loop() {}
